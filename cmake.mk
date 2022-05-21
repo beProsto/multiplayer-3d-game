368ee75008a8f48b06d68287b58579e79777f7ee
beProsto <lavionperavion2@gmail.com>
2022-05-21 21:10:43 +0200

    It actually seems to work now UwU

--- cmake.mk
+++ cmake.mk
@@ -1,5 +1,11 @@
 CC  = gcc
 CXX = g++
 
-buildexe:
-	cmake -B ./build -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug "-DCMAKE_C_COMPILER:FILEPATH=$(CC)" "-DCMAKE_CXX_COMPILER:FILEPATH=$(CXX)" -G Ninja & cmake --build ./build
+all: clean
+	make -f cmake.mk compile
+
+compile:
+	cmake -B ./build -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug "-DCMAKE_C_COMPILER:FILEPATH=$(CC)" "-DCMAKE_CXX_COMPILER:FILEPATH=$(CXX)" -G "Unix Makefiles" & cmake --build ./build
+
+clean:
+	cd build & git clean -d -f -x
